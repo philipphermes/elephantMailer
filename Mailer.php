df3acc6fa9f669315f27816c5f6cdca0de54959a
Philipp Anton Hermes <philipphermes2001@gmail.com>
2022-12-06 10:18:02 +0100

    first commit

---
+++ Mailer.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+class Mailer
+{
+    private static string $mailTo;
+    private static string $mailFrom;
+    private static string $subject;
+    private static array $data;
+
+    public const H1 = 1;
+    public const H2 = 2;
+    public const H3 = 3;
+    public const H4 = 4;
+
+    public const UNORDERED_LIST = "ul";
+    public const ORDERED_LIST = "ol";
+
+    /**
+     * @param string $mailTo
+     * @return void
+     * The email of the receiver
+     */
+    public static function setEmailTo(string $mailTo): void {
+        self::$mailTo = $mailTo;
+    }
+
+    /**
+     * @param string $mailFrom
+     * @return void
+     * The email of the sender
+     */
+    public static function setEmailFrom(string $mailFrom): void {
+        self::$mailFrom = $mailFrom;
+    }
+
+    public static function setSubject(string $subject): void {
+        self::$subject = $subject;
+    }
+
+    public static function addTitle(string $title, int $titleType = self::H1): void {
+        self::$data[] = sprintf("<h%s>%s</h%s>", $titleType, $title, $titleType);
+    }
+
+    public static function addParagraph(string $paragraph): void {
+        self::$data[] = sprintf("<p>%s</p>", $paragraph);
+    }
+
+    public static function addList(array $list, string $listType = self::UNORDERED_LIST): void {
+        if (empty($list)) {
+            throw new RuntimeException("List is empty!");
+        }
+
+        self::$data[] = sprintf("<%s>", $listType);
+
+        foreach ($list as $element) {
+            self::$data[] = sprintf("<li>%s</li>", $element);
+        }
+
+        self::$data[] = sprintf("</%s>", $listType);
+    }
+
+    public function sendMail(): void
+    {
+        //TODO
+    }
+}
